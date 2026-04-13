4323acb9873df71c3e0ef7442900b4ff0c3f9c99
Application-Saab <harsha10889@gmail.com>
2026-04-13 17:38:33 +0530

    Bse URL issue fix

--- hora-new-admin-dashboard/src/app/component/ActionPop.js
+++ hora-new-admin-dashboard/src/app/component/ActionPop.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import * as XLSX from "xlsx"; 
 import CallChecklistContent from "./CallChecklistContent";
+import { BASE_URL } from "@/utils/apiconstant";
 
 const categoryMap = {
   "65a92271ae1586258ccd0628": "anniversary-decoration",
